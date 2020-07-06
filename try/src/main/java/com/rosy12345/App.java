e90d235cca1fb317d9856f293bbb47205a3663da
ROSY12345 <roserosy894@gmail.com>
2020-07-06 09:09:26 +0000

    adding maven files

---
+++ try/src/main/java/com/rosy12345/App.java
@@ -0,0 +1,13 @@
+package com.rosy12345;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
